efdab3be2110412278209beb6d36d6c0105f270e
MongoPong0 <vlorente@alu.ucam.edu>
2017-12-11 22:58:55 +0100

    Mejora a Agenda de series, solo se listarán aquellas que se encuentren en emisión.

--- app/src/main/java/android/ucam/edu/seiries/ActivitiAddSerie.java
+++ app/src/main/java/android/ucam/edu/seiries/ActivitiAddSerie.java
@@ -334,29 +334,21 @@ public class ActivitiAddSerie extends AppCompatActivity {
     //Solicitar permisos de lectura y escrituda en calendario
     //en tiempo de ejecución
     private void calendarPermissions() {
-        // Here, thisActivity is the current activity
+
         if (ContextCompat.checkSelfPermission(ActivitiAddSerie.this,
                 Manifest.permission.WRITE_CALENDAR)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
+
             if (ActivityCompat.shouldShowRequestPermissionRationale(ActivitiAddSerie.this,
                     Manifest.permission.WRITE_CALENDAR) || ActivityCompat.shouldShowRequestPermissionRationale(ActivitiAddSerie.this,
                     Manifest.permission.READ_CALENDAR)) {
 
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
 
             } else {
 
-                // No explanation needed, we can request the permission.
-
                 ActivityCompat.requestPermissions(ActivitiAddSerie.this, PERMISSIONS, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
 
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         }
     }
